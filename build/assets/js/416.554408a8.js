b3c083377a0bbba1129dfc59b37ece165da514a9
kundan.kumar@saviynt.com <Kundan.thelife@hotmail.com>
2024-04-04 19:09:56 -0400

    updates

--- build/assets/js/416.554408a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[416],{90416:(e,s,k)=>{k.r(s)}}]);
--- build/assets/js/8913.0827c498.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[8913],{58913:(e,s,k)=>{k.r(s)}}]);
