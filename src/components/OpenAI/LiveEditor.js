fdad5a61ece12a159f5e07495f26ef0828d2becb
kundan.kumar@saviynt.com <Kundan.thelife@hotmail.com>
2024-04-29 18:44:31 -0400

    removed liveEditor Component

--- src/components/OpenAI/LiveEditor.js
+++ src/components/OpenAI/LiveEditor.js
@@ -3,25 +3,25 @@ import React from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 const LiveEditorComponent = ({ code, scope }) => {
-  const [output, setOutput] = useState('');
+  // const [output, setOutput] = useState('');
 
-  const runCode = async () => {
-    try {
-      const response = await axios.post('/api/openai', { prompt: code });
-      setOutput(response.data.choices[0].text);
-    } catch (error) {
-      setOutput('Error: ' + error.message);
-    }
-  };
+  // const runCode = async () => {
+  //   try {
+  //     const response = await axios.post('/api/openai', { prompt: code });
+  //     setOutput(response.data.choices[0].text);
+  //   } catch (error) {
+  //     setOutput('Error: ' + error.message);
+  //   }
+  // };
 
-  return (
-    <LiveProvider code={code}>
-      <LiveEditor onChange={runCode} />
-      <LivePreview />
-      <div>{output}</div>
-      <LiveError />
-    </LiveProvider>
-  );
+  // return (
+  //   <LiveProvider code={code}>
+  //     <LiveEditor onChange={runCode} />
+  //     <LivePreview />
+  //     <div>{output}</div>
+  //     <LiveError />
+  //   </LiveProvider>
+  // );
 };
 
 export default LiveEditorComponent;
