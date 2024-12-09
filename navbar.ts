062e03712553da014a7b87b37ad67f8dff4e7195
John Wang <johncwang@gmail.com>
2024-12-08 20:16:24 -0800

    refactor: `navbar.ts`: update spacing

--- navbar.ts
+++ navbar.ts
@@ -1,33 +1,33 @@
 import type { Navbar } from '@docusaurus/theme-common';
 
-const navbar: Navbar ={
-    title: "FOR DEVELOPERS",
-    logo: {
-      alt: "SAVIYNT",
-      src: "img/Saviynt_logo_Navy.svg",
-      srcDark: 'img/Saviynt_logo_White.svg',
+const navbar: Navbar = {
+  title: "FOR DEVELOPERS",
+  logo: {
+    alt: "SAVIYNT",
+    src: "img/Saviynt_logo_Navy.svg",
+    srcDark: 'img/Saviynt_logo_White.svg',
+  },
+  hideOnScroll: false,  // Set to true or false based on your preference
+  items: [
+    {
+      label: "📖 GUIDE",
+      to: "/guide",
     },
-    hideOnScroll: false,  // Set to true or false based on your preference
-    items: [
-      {
-        label: "📖 GUIDE",
-        to: "/guide",
-      },
-      {
-        label: "👥 COMMUNITY",
-        to: "/community",
-      },
-      {
-        label: "📰 BLOGS",
-        to: "/blog/",
-      },
-      {
-        href: "https://github.com/saviynt",
-        position: "right",
-        className: "header-github-link",
-        "aria-label": "GitHub repository",
-      },
-    ],
-  }
+    {
+      label: "👥 COMMUNITY",
+      to: "/community",
+    },
+    {
+      label: "📰 BLOGS",
+      to: "/blog/",
+    },
+    {
+      href: "https://github.com/saviynt",
+      position: "right",
+      className: "header-github-link",
+      "aria-label": "GitHub repository",
+    },
+  ],
+}
 
-  export default navbar;
+export default navbar;
